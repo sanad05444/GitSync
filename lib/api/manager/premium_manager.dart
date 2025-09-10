569714ea5fab2c0389697e8c443229ab3fd11d34
ViscousPot <viscouspotential@gmail.com>
2025-09-10 06:31:21 +0100

    feat: use new gitsync sponsors logic

--- lib/api/manager/premium_manager.dart
+++ lib/api/manager/premium_manager.dart
@@ -31,9 +31,7 @@ class PremiumManager {
       return;
     }
 
-    final userToken = await repoManager.getStringNullable(
-      StorageKey.repoman_ghSponsorToken,
-    );
+    final userToken = await repoManager.getStringNullable(StorageKey.repoman_ghSponsorToken);
     if (userToken == null) {
       await repoManager.setBool(StorageKey.repoman_hasGHSponsorPremium, false);
       hasPremiumNotifier.value = await _readPremiumStatus();
@@ -42,10 +40,7 @@ class PremiumManager {
 
     final userRes = await http.get(
       Uri.parse('https://api.github.com/user'),
-      headers: {
-        'Authorization': 'token $userToken',
-        'Accept': 'application/vnd.github.v3+json',
-      },
+      headers: {'Authorization': 'token $userToken', 'Accept': 'application/vnd.github.v3+json'},
     );
 
     if (userRes.statusCode != 200) {
@@ -56,15 +51,7 @@ class PremiumManager {
 
     final userNodeId = jsonDecode(userRes.body)['node_id'].toString();
 
-    final fileRes = await http.get(
-      Uri.parse(
-        'https://api.github.com/repos/ViscousPot/sponsors/contents/sponsors.txt',
-      ),
-      headers: {
-        'Authorization': 'token $gitHubSponsorsToken',
-        'Accept': 'application/vnd.github.v3.raw',
-      },
-    );
+    final fileRes = await http.get(Uri.parse('https://raw.githubusercontent.com/ViscousPot/sponsors-gitsync/refs/heads/main/sponsors.txt'));
 
     if (userNodeId.isEmpty || fileRes.statusCode != 200) {
       await repoManager.setBool(StorageKey.repoman_hasGHSponsorPremium, false);
@@ -76,26 +63,19 @@ class PremiumManager {
     final lines = LineSplitter.split(content).map((e) => e.trim()).toList();
     final isSponsor = lines.contains(userNodeId);
 
-    await repoManager.setBool(
-      StorageKey.repoman_hasGHSponsorPremium,
-      isSponsor,
-    );
+    await repoManager.setBool(StorageKey.repoman_hasGHSponsorPremium, isSponsor);
     hasPremiumNotifier.value = await _readPremiumStatus();
   }
 
   Future<void> cullNonPremium() async {
-    final repomanReponames = await repoManager.getStringList(
-      StorageKey.repoman_repoNames,
-    );
+    final repomanReponames = await repoManager.getStringList(StorageKey.repoman_repoNames);
     if (repomanReponames.length > 1) {
       List.generate(repomanReponames.length - 1, (index) async {
         final manager = await SettingsManager().reinit(repoIndex: 1 + index);
         await manager.clearAll();
       });
       await repoManager.setInt(StorageKey.repoman_repoIndex, 0);
-      await repoManager.setStringList(StorageKey.repoman_repoNames, [
-        repomanReponames[0],
-      ]);
+      await repoManager.setStringList(StorageKey.repoman_repoNames, [repomanReponames[0]]);
     }
   }
 
