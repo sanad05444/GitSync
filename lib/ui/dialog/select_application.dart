c6234e63f65a516e05a17e19a36e5b0961c0c834
ViscousPot <viscouspotential@gmail.com>
2025-09-16 17:05:58 +0100

    chore: improve app selection ui speed

--- lib/ui/dialog/select_application.dart
+++ lib/ui/dialog/select_application.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:GitSync/api/manager/storage.dart';
 import 'package:flutter/material.dart' as mat;
 import 'package:flutter/material.dart';
@@ -9,15 +11,14 @@ import '../../../constant/colors.dart';
 import '../../../constant/dimens.dart';
 import '../../../global.dart';
 import '../../../ui/dialog/base_alert_dialog.dart';
+import 'package:flutter/services.dart';
 import 'package:async/async.dart';
 
-Future<void> showDialog(
-  BuildContext parentContext,
-  Set<String>? prevSelectedApplications,
-) async {
-  final List<String> selectedApplications =
-      prevSelectedApplications?.toList() ?? [];
+Future<void> showDialog(BuildContext parentContext, Set<String>? prevSelectedApplications) async {
+  final List<String> selectedApplications = prevSelectedApplications?.toList() ?? [];
   final searchController = TextEditingController();
+  final Map<String, Uint8List?> iconCache = {};
+  final Map<String, String> labelCache = {};
 
   return await mat.showDialog(
     context: parentContext,
@@ -27,197 +28,152 @@ Future<void> showDialog(
         child: Text(
           t.selectApplication.toUpperCase(),
           textAlign: TextAlign.center,
-          style: TextStyle(
-            color: primaryLight,
-            fontSize: textXL,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(color: primaryLight, fontSize: textXL, fontWeight: FontWeight.bold),
         ),
       ),
       content: StatefulBuilder(
-        builder: (BuildContext context, setState) => SingleChildScrollView(
-          child: ListBody(
-            children: [
-              TextField(
-                controller: searchController,
-                maxLines: 1,
-                style: TextStyle(
-                  color: primaryLight,
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.none,
-                  decorationThickness: 0,
-                  fontSize: textMD,
-                ),
-                decoration: InputDecoration(
-                  fillColor: tertiaryDark,
-                  filled: true,
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(cornerRadiusMD),
-                    borderSide: BorderSide.none,
-                  ),
-                  isCollapsed: true,
-                  label: Text(
-                    t.search.toUpperCase(),
-                    style: TextStyle(
-                      color: secondaryLight,
-                      fontSize: textSM,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: spaceMD,
-                    vertical: spaceSM,
-                  ),
-                  isDense: true,
-                ),
-                onChanged: (value) {
-                  debounce(selectApplicationSearchReference, 100, () {
-                    setState(() {});
-                  });
-                },
-              ),
-              SizedBox(height: spaceMD),
-              FutureBuilder(
-                future: AsyncMemoizer().runOnce(
-                  () => AccessibilityServiceHelper.getDeviceApplications(
-                    searchController.text,
-                  ),
-                ),
-                builder: (context, deviceAppsSnapshot) {
-                  final packageNames = <String>{
-                    ...selectedApplications,
-                    ...deviceAppsSnapshot.data ?? [],
-                  }.toList();
+        builder: (BuildContext context, setState) {
+          Future<List<String>> deviceApplicationsFuture = AccessibilityServiceHelper.getDeviceApplications(searchController.text);
 
-                  return SizedBox(
-                    width: double.maxFinite,
-                    height: MediaQuery.of(context).size.height / 3,
-                    child: deviceAppsSnapshot.data == null
-                        ? Center(
-                            child: CircularProgressIndicator(
-                              color: tertiaryLight,
-                            ),
-                          )
-                        : GridView.builder(
-                            shrinkWrap: true,
-                            itemCount: packageNames.length,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  crossAxisSpacing: spaceMD,
-                                  mainAxisSpacing: spaceMD,
-                                ),
-                            itemBuilder: (BuildContext context, int index) {
-                              final packageName = packageNames[index];
-                              return Stack(
-                                key: Key(packageName),
-                                children: [
-                                  TextButton(
-                                    onPressed: () {
-                                      if (selectedApplications.contains(
-                                        packageName,
-                                      )) {
-                                        selectedApplications.remove(
-                                          packageName,
-                                        );
-                                      } else {
-                                        selectedApplications.add(packageName);
-                                      }
-                                      setState(() {});
-                                    },
-                                    style: ButtonStyle(
-                                      alignment: Alignment.centerLeft,
-                                      backgroundColor: WidgetStatePropertyAll(
-                                        tertiaryDark,
+          return SingleChildScrollView(
+            child: ListBody(
+              children: [
+                TextField(
+                  controller: searchController,
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: primaryLight,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.none,
+                    decorationThickness: 0,
+                    fontSize: textMD,
+                  ),
+                  decoration: InputDecoration(
+                    fillColor: tertiaryDark,
+                    filled: true,
+                    border: const OutlineInputBorder(borderRadius: BorderRadius.all(cornerRadiusMD), borderSide: BorderSide.none),
+                    isCollapsed: true,
+                    label: Text(
+                      t.search.toUpperCase(),
+                      style: TextStyle(color: secondaryLight, fontSize: textSM, fontWeight: FontWeight.bold),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    contentPadding: const EdgeInsets.symmetric(horizontal: spaceMD, vertical: spaceSM),
+                    isDense: true,
+                  ),
+                  onChanged: (value) {
+                    debounce(selectApplicationSearchReference, 100, () {
+                      deviceApplicationsFuture = AccessibilityServiceHelper.getDeviceApplications(value);
+                      setState(() {});
+                    });
+                  },
+                ),
+                SizedBox(height: spaceMD),
+                FutureBuilder(
+                  future: AsyncMemoizer().runOnce(() async => await deviceApplicationsFuture),
+                  builder: (context, deviceAppsSnapshot) {
+                    final packageNames = <String>{...selectedApplications, ...deviceAppsSnapshot.data ?? []}.toList();
+
+                    return SizedBox(
+                      width: double.maxFinite,
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: deviceAppsSnapshot.data == null
+                          ? Center(child: CircularProgressIndicator(color: tertiaryLight))
+                          : GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: packageNames.length,
+                              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                crossAxisSpacing: spaceMD,
+                                mainAxisSpacing: spaceMD,
+                              ),
+                              itemBuilder: (BuildContext context, int index) {
+                                final packageName = packageNames[index];
+
+                                Future<Uint8List?> iconFuture;
+                                if (iconCache.containsKey(packageName)) {
+                                  iconFuture = Future.value(iconCache[packageName]!);
+                                } else {
+                                  iconFuture = AccessibilityServiceHelper.getApplicationIcon(packageName).then((bytes) {
+                                    iconCache[packageName] = bytes;
+                                    return bytes;
+                                  });
+                                }
+
+                                Future<String> labelFuture;
+                                if (labelCache.containsKey(packageName)) {
+                                  labelFuture = Future.value(labelCache[packageName]!);
+                                } else {
+                                  labelFuture = AccessibilityServiceHelper.getApplicationLabel(packageName).then((label) {
+                                    labelCache[packageName] = label;
+                                    return label;
+                                  });
+                                }
+
+                                return Stack(
+                                  key: Key(packageName),
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        if (selectedApplications.contains(packageName)) {
+                                          selectedApplications.remove(packageName);
+                                        } else {
+                                          selectedApplications.add(packageName);
+                                        }
+                                        setState(() {});
+                                      },
+                                      style: ButtonStyle(
+                                        alignment: Alignment.centerLeft,
+                                        backgroundColor: WidgetStatePropertyAll(tertiaryDark),
+                                        padding: WidgetStatePropertyAll(EdgeInsets.all(spaceSM)),
+                                        shape: WidgetStatePropertyAll(
+                                          RoundedRectangleBorder(borderRadius: BorderRadius.all(cornerRadiusMD), side: BorderSide.none),
+                                        ),
                                       ),
-                                      padding: WidgetStatePropertyAll(
-                                        EdgeInsets.all(spaceSM),
-                                      ),
-                                      shape: WidgetStatePropertyAll(
-                                        RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.all(
-                                            cornerRadiusMD,
-                                          ),
-                                          side: BorderSide.none,
+                                      child: Center(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            SizedBox(
+                                              height: textXXL,
+                                              width: textXXL,
+                                              child: FutureBuilder(
+                                                future: iconFuture,
+                                                builder: (context, snapshot) => snapshot.data == null
+                                                    ? CircularProgressIndicator(color: tertiaryLight)
+                                                    : Image.memory(snapshot.data!, height: textXXL, width: textXXL, gaplessPlayback: true),
+                                              ),
+                                            ),
+                                            SizedBox(height: spaceSM),
+                                            FutureBuilder(
+                                              future: labelFuture,
+                                              builder: (context, snapshot) => Text(
+                                                (snapshot.data ?? "").toUpperCase(),
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(color: primaryLight, fontSize: textSM, fontWeight: FontWeight.bold),
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
                                     ),
-                                    child: Center(
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          SizedBox(
-                                            height: textXXL,
-                                            width: textXXL,
-                                            child: FutureBuilder(
-                                              future: AsyncMemoizer().runOnce(
-                                                () async =>
-                                                    AccessibilityServiceHelper.getApplicationIcon(
-                                                      packageName,
-                                                    ),
-                                              ),
-                                              builder: (context, snapshot) =>
-                                                  snapshot.data == null
-                                                  ? CircularProgressIndicator(
-                                                      color: tertiaryLight,
-                                                    )
-                                                  : Image.memory(
-                                                      height: textXXL,
-                                                      width: textXXL,
-                                                      gaplessPlayback: true,
-                                                      snapshot.data!,
-                                                    ),
-                                            ),
-                                          ),
-                                          SizedBox(height: spaceSM),
-                                          FutureBuilder(
-                                            future: AsyncMemoizer().runOnce(
-                                              () async =>
-                                                  AccessibilityServiceHelper.getApplicationLabel(
-                                                    packageName,
-                                                  ),
-                                            ),
-                                            // future: AccessibilityServiceHelper.getApplicationLabel(packageName),
-                                            builder: (context, snapshot) =>
-                                                Text(
-                                                  (snapshot.data ?? "")
-                                                      .toUpperCase(),
-                                                  textAlign: TextAlign.center,
-                                                  style: TextStyle(
-                                                    color: primaryLight,
-                                                    fontSize: textSM,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                          ),
-                                        ],
+                                    if (selectedApplications.contains(packageName))
+                                      Positioned(
+                                        top: spaceSM,
+                                        right: spaceSM,
+                                        child: FaIcon(FontAwesomeIcons.solidCircleCheck, color: primaryPositive, size: textXL),
                                       ),
-                                    ),
-                                  ),
-                                  ...selectedApplications.contains(packageName)
-                                      ? [
-                                          Positioned(
-                                            top: spaceSM,
-                                            right: spaceSM,
-                                            child: FaIcon(
-                                              FontAwesomeIcons.solidCircleCheck,
-                                              color: primaryPositive,
-                                              size: textXL,
-                                            ),
-                                          ),
-                                        ]
-                                      : [],
-                                ],
-                              );
-                            },
-                          ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
+                                  ],
+                                );
+                              },
+                            ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        },
       ),
       actions: <Widget>[
         TextButton(
@@ -235,10 +191,7 @@ Future<void> showDialog(
             style: TextStyle(color: primaryPositive, fontSize: textMD),
           ),
           onPressed: () async {
-            uiSettingsManager.setStringList(
-              StorageKey.setman_packageNames,
-              selectedApplications,
-            );
+            uiSettingsManager.setStringList(StorageKey.setman_packageNames, selectedApplications);
             Navigator.of(context).canPop() ? Navigator.pop(context) : null;
           },
         ),
